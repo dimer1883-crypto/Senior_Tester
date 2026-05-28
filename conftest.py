133be0dda73d6ba2cbff61aad8aefb43876b9de4
d.erakhtin <dimer1883@yandex.ru>
2026-05-28 09:37:20 +0400

    test ready

---
+++ conftest.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import pytest
+from selenium.webdriver.firefox.options import Options
+
+
+@pytest.fixture
+def browser():
+    options = Options()
+    options.add_argument('-headless')
+    browser = webdriver.Firefox(options=options)
+    browser.maximize_window()
+    browser.implicitly_wait(2)
+    yield browser
+    browser.quit()
