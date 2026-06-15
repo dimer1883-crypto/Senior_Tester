45886be04352e2e5dea82787b97c7ff1de3c49e3
d.erakhtin <dimer1883@yandex.ru>
2026-06-15 10:06:34 +0400

    поправил файл conftest.py для запуска тестов через guthab

--- conftest.py
+++ conftest.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 import pytest
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def browser():
     options = Options()
-    options.add_argument('-headless')
-    browser = webdriver.Firefox(options=options)
+    # Аргументы специально для GitHub Actions (Linux)
+    options.add_argument('--headless=new')  # Запуск без окна
+    options.add_argument('--no-sandbox')  # Обязательно для CI (Ubuntu)
+    options.add_argument('--disable-dev-shm-usage')  # Чтобы не падало по памяти
+    options.add_argument('--window-size=1920,1080')  # Полноэкранный режим
+
+    browser = webdriver.Chrome(options=options)  # Инициализируем Chrome
     browser.maximize_window()
-    browser.implicitly_wait(2)
+    browser.implicitly_wait(5)  # Чуть увеличил ожидание для стабильности
     yield browser
     browser.quit()
